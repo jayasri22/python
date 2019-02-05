da8957e834273666014fe4ab49b4cfacfad84437
jayasri22 <47314266+jayasri22@users.noreply.github.com>
2019-02-05 12:41:30 +0530

    Create vow.py

---
+++ vow.py
@@ -0,0 +1,7 @@
+ch=input("enter a char")
+    if(ch=='A' or ch=='a' or ch=='E' or ch =='e' or ch=='I'
+    or ch=='i' or ch=='O' or ch=='o' or ch=='U' or ch=='u'):
+        print(ch, "Vowel")
+    else:
+        print(ch, " Consonant")
+    
