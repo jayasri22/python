ec203bdd663ecc16be7b7ae3e41cb39061ed04f5
jayasri22 <47314266+jayasri22@users.noreply.github.com>
2019-02-05 14:58:08 +0530

    Create hello.c

---
+++ hello.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<conio.h>
+int main(void)
+{
+int k,n;
+  printf("enter n value:");
+  scanf("%d",&n);
+  for(k=0;k<n;k++)
+  {
+    printf("hello\n ");
+  }
+getch();
+}
