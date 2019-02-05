17ecb0902ffed593638838f42b63ec97d68cf64d
jayasri22 <47314266+jayasri22@users.noreply.github.com>
2019-02-05 14:32:56 +0530

    Create large.py

---
+++ large.py
@@ -0,0 +1,12 @@
+print("enter the 3  no:")
+ 
+n1 = int(input())
+n2 = int(input())
+n3= int(input())
+ 
+if n1>n2 and n1>n3:
+	print(n1, "largest")
+elif n2>n3 and n2>n3:
+	print(n2, "largest")
+else:
+	print(n3, "largest")
